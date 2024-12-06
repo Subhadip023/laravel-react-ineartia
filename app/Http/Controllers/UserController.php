4f8594b96dd7d5ed158029f26a01b79adab80af5
Subhadip023 <subhadip240420@gmail.com>
2024-12-06 12:30:40 +0530

    make some changes in controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
      */
     public function index()
     {
-       $users=User::all();
+        $users = User::with('roles', 'permissions')->get();
         if (auth()->user()->hasPermissionTo( 'show users')) {
             return inertia('Admin/User',['users'=>$users]);
         }
--- resources/js/Pages/Admin/User.jsx
+++ resources/js/Pages/Admin/User.jsx
@@ -1,10 +1,64 @@
 import React from 'react'
 
-function User({users}) {
-console.log(users)
+function User({ users }) {
+  console.log(users)
 
+  let index = 0;
   return (
-    <div> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias amet eum dolorum nostrum voluptatem atque corrupti doloribus minus pariatur facilis at neque aperiam, nemo cum, optio quidem perspiciatis rerum quae tempore nesciunt aut? Dignissimos esse deleniti inventore facere dolorem tenetur sapiente, quae, fugit quisquam quo iusto temporibus ipsa animi excepturi distinctio recusandae velit vel. Omnis quam eius quae aspernatur cupiditate ut aut reiciendis impedit repellat voluptas atque deleniti molestias nam, unde voluptatibus magni consequuntur voluptatum eum minima! Quae fugiat, rerum, recusandae numquam ea autem itaque dolor nobis expedita possimus enim, architecto voluptate porro! Veniam facere sed, voluptate enim nostrum reiciendis, quas culpa libero possimus assumenda laboriosam consectetur eum sint ipsum! At corporis saepe pariatur consequuntur ipsam, nisi provident molestias repellendus aliquid eum obcaecati maiores architecto facilis quas iusto explicabo possimus nesciunt delectus blanditiis? Enim eum maiores quasi quo obcaecati iure. Nostrum consectetur, autem voluptates dolor, dolores saepe doloremque delectus perferendis hic cumque earum, nisi voluptatum aliquam architecto beatae aliquid. Tempore dolorem, qui, suscipit, explicabo vitae fuga pariatur sint commodi ad voluptatum nulla facere asperiores minima aliquam illo doloremque voluptate? Ea est maxime odio sequi sapiente dolores provident natus? Aut incidunt inventore maxime alias ullam nostrum. Asperiores optio molestiae nesciunt iusto assumenda eveniet repellendus, cum porro a numquam saepe consequatur omnis veritatis vero, magni enim temporibus excepturi ipsum necessitatibus molestias corporis voluptatem, ipsa ipsam ratione. Iusto sunt aut porro voluptatum ullam facere fugiat necessitatibus eius repellat, modi voluptatem pariatur possimus consequuntur accusamus iure. Quidem doloremque, itaque eos, unde nobis consequuntur error fugiat id est dolore at accusantium a obcaecati delectus possimus. A, exercitationem quos atque quasi beatae molestias illo soluta sunt repudiandae cumque vel ducimus aperiam quod esse error unde excepturi iste delectus iure. Quis deleniti, ea eveniet quo similique modi molestias atque repudiandae ipsam quam, recusandae quidem provident. Omnis, quasi!</div>
+
+    <>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-20">
+            <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Pricing</h1>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Banh mi cornhole echo park skateboard authentic crucifix neutra tilde lyft biodiesel artisan direct trade mumblecore 3 wolf moon twee</p>
+          </div>
+          <div className="lg:w-2/3 w-full mx-auto overflow-auto">
+            <table className="table-auto w-full text-left whitespace-no-wrap">
+              <thead>
+                <tr>
+                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">#</th>
+                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Name</th>
+                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Email</th>
+                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Roles</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  users.map((user) => (
+                    <tr>
+                      <td className="px-4 py-3">{index++}</td>
+                      <td className="px-4 py-3">{user.name}</td>
+                      <td className="px-4 py-3">{user.email}</td>
+                      <td className="px-4 py-3 text-lg text-gray-900">
+                        {
+                          users.roles.lenght === 0 ? 'No Roles assign to the user' :user.roles?.map((permission) => permission.name).join(", ")}
+
+
+                      </td>
+
+                    </tr>
+                  ))
+                }
+
+
+
+              </tbody>
+            </table>
+          </div>
+          <div className="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
+            <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </a>
+            <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+          </div>
+        </div>
+      </section>
+
+    </>
+
   )
 }
 
